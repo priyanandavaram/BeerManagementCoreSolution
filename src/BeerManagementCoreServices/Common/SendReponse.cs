a5dd8dececc17dc5dec8d6473c6e72e3b2f570b9
lakshmi.nandavaram1 <lakshmi.nandavaram1@nhs.net>
2023-09-05 22:27:16 +0100

    Changes

--- src/BeerManagementCoreServices/Common/SendReponse.cs
+++ src/BeerManagementCoreServices/Common/SendReponse.cs
@@ -2,7 +2,7 @@
 namespace BeerManagement.Web.Common
 {
     public class SendReponse
-    {    
+    {
         public static IActionResult ApiResponse(object result)
         {
             return new OkObjectResult(new ApiAttributes<object>
@@ -31,26 +31,11 @@ namespace BeerManagement.Web.Common
             });
         }
 
-        public static IActionResult BadRequestObjectResult(string input)
-        {
-            return new BadRequestObjectResult(new ApiAttributes<object>
-            {
-                StatusCode = 400,
-                Message = $"Provided {input} input is invalid."
-            });
-        }
-        public static IActionResult InternalServerError()
-        {
-            return new ObjectResult(new ApiAttributes<object>
-            {
-                StatusCode = 500,
-                Message = "Unable to complete the operation due to invalid id or due to internal error."
-            });
-        }
         public static IActionResult BadRequest()
         {
             return new BadRequestResult();
         }
+
         public static IActionResult NoContentFound()
         {
             return new NotFoundResult();
